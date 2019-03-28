4c7b2662f3ba1c2a7430447932e5e45e8dbd292d
Mike Silvers <mike.silvers@buckettechnologies.com>
2019-03-28 11:42:10 -0400

    Working on the documentation

--- TWSimulator/Models/TWUser.swift
+++ TWSimulator/Models/TWUser.swift
@@ -8,11 +8,22 @@
 
 import Foundation
 
-// Note for reviewers: I am using NSObject and NSCoding here because we are saving the
-// TWUser object in the keychain for security.  Saving an object in the keychain requires
-// the NSObject and NSCoding protocols.
+/**
+ A model of the user information.
+ 
+ This `struct` is a model of the TWUser`.  The model extends the `NSObject` base object to allow the object to be saved inthe keychain.
+ 
+ The model implements the `NSCoding` protocol allowing for an easy method to determine if two users are the same.  The user comparison is based on the unique `ID` for each user.
+
+ - Note: We use NSObject and NSCoding here because we are saving the `TWUser` object in the keychain for security.  Saving an object in the keychain requires the NSObject and NSCoding protocols.
+
+ - Author: Mike Silvers
+ - Date: 3/25/19
+ */
+
 class TWUser : NSObject, NSCoding {
     
+    //MARK: Variable definitions
     var userName          : String = "TW User"
     var firstName         : String?
     var lastName          : String?
@@ -20,6 +31,10 @@ class TWUser : NSObject, NSCoding {
     var lastLoggedInTime  : Int = 0
     var lastLoggedOutTime : Int = 0
 
+    //MARK: Supporting enum
+    /**
+     This `enum` defines the coding keys for the encoding/decoding of information to/from JSON.
+     */
     enum Keys: String {
         case userName          = "UserName"
         case firstName         = "FirstName"
@@ -28,6 +43,7 @@ class TWUser : NSObject, NSCoding {
         case lastLoggedOutTime = "LastLoggedOutTime"
     }
     
+    //MARK: NSCodable Protocol
     func encode(with aCoder: NSCoder) {
         
         aCoder.encode(userName, forKey: Keys.userName.rawValue)
@@ -37,9 +53,18 @@ class TWUser : NSObject, NSCoding {
         aCoder.encode(lastLoggedOutTime, forKey: Keys.lastLoggedOutTime.rawValue)
         
     }
-    
+
+    //MARK: Initializers
+    /**
+     The default required initializer to confrm to the decoding protocol.
+     
+     This initializer processes the decoding to create an object when the decoder is passed as a parameter.
+     
+     - Parameter aDecoder: `NSCoder` object to be processed
+    */
     required init?(coder aDecoder: NSCoder) {
         
+        // process the attributes if they exist in the `NSCoder` object.
         if let data = aDecoder.decodeObject(forKey: Keys.userName.rawValue) as? String {
             userName = data
         }
@@ -57,13 +82,31 @@ class TWUser : NSObject, NSCoding {
 
     }
 
+    /**
+     
+     An `init` function containing all attributes.
+     
+     This `init` function provides an option to create a `Tweet` while providing all attriutes
+     
+     - Parameter username: The user username
+     - Parameter firstname: The user first name
+     - Parameter lastname: The user last name
+     - Parameter lastloggedintime: The last time the user has successfullt logged in
+     */
+
     init (username: String, firstname: String, lastname: String, lastloggedintime: Int) {
+        // set the object parameters according to the initializer parameters
         userName = username
         firstName = firstname
         lastName = lastname
         lastLoggedInTime = lastloggedintime
     }
     
+    /**
+     The default initializer as required.
+     
+     Provides a default initializer with no parameters.  This allows us to create a base object.  No configuration is required as there are no default values for the user.
+    */
     override init() { super.init() }
     
 }
--- TWSimulator/Models/Tweet.swift
+++ TWSimulator/Models/Tweet.swift
@@ -8,24 +8,29 @@
 
 import Foundation
 
+/**
+ A model of a message.
+ 
+ This `struct` is a model of the `Tweet` data.  The model implements the `Codable` protocol to allow for easy transformation to and from JSON.
+ 
+ The model implements the `Comparable` protocol allowing for an easy method to determine if two `Tweet` items are the same.  The comparison is based on the unique `ID` for each `Tweet`.
+ 
+ - Author: Mike Silvers
+ - Date: 3/25/19
+ */
 struct Tweet : Codable, Comparable {
     
-    static func < (lhs: Tweet, rhs: Tweet) -> Bool {
-        
-        var compared = true
-        
-        // we only check the message ID as the other information may be different
-        if lhs.messageId != rhs.messageId, compared { compared = false }
-
-        return compared
-    }
-    
+    //MARK: Variable definitions
     var messageId       : Int = 0
     var createdTimeDate : Int = 0
     var readTimeDate    : Int?
     
     var message         : String
     
+    //MARK: Supporting enumerations
+    /**
+     This `enum` defines the coding keys for the encoding/decoding of information to/from JSON.
+    */
     enum CodingKeys: String, CodingKey {
         
         case messageId       = "message_id"
@@ -34,7 +39,38 @@ struct Tweet : Codable, Comparable {
 
         case message
     }
+
+    //MARK: Comparable protocol functions
+    /**
+     
+     This `static` function compares two objects to determine if they are not equal.
+     
+     The attribute determining equality is the `ID` for the item.
+     
+     - Parameter lhs: the initial item for comparison (this item)
+     - Parameter rhs: the item to compare this item to
+     - Returns: a `Bool` that indicates of the comparison is **true** or **false**
+    */
+    static func < (lhs: Tweet, rhs: Tweet) -> Bool {
+        
+        // we start by assuming that the two items are equal
+        var compared = true
+        
+        // we only check the message ID as the other information may be different
+        if lhs.messageId != rhs.messageId, compared { compared = false }
+        
+        // Quick note: We sould just return the `true` or `false` if conditions are met, but the current configuration allows for multiple test to be performed easily by using a `compared` variable.
+        return compared
+    }
     
+    //MARK: Support functions
+    /**
+     
+     This `private` function assigns the unique `ID` for this message.
+     
+     The attribute `ID` is set for the item.
+     
+     */
     private mutating func setID() {
         
         // set the ID if the ID is 0
@@ -43,15 +79,40 @@ struct Tweet : Codable, Comparable {
         }
     }
     
+    //MARK: Initialization functions
+    /**
+     
+     An `init` function containing the message only.
+     
+     This `init` function provides an option to create a `Tweet` while providing only the new message
+     
+     - Parameter newMessage: The message to be saved for this `Tweet`
+     
+     */
     init(_ newMessage: String) {
         
+        // configure the required parameters
         messageId = TWDataSource.shared.nextIdNumber()
         createdTimeDate = Int(Date().timeIntervalSince1970)
         readTimeDate = 0
+        
+        // add the message to the `Tweet`
         message = newMessage
         
     }
     
+    /**
+     
+     An `init` function containing all attributes.
+     
+     This `init` function provides an option to create a `Tweet` while providing all attriutes
+     
+     - Parameter messageId: The unique ID to be saved for this `Tweet`
+     - Parameter createdTimeDate: The time and date created (in Epoch format) to be saved for this `Tweet`
+     - Parameter readTimeDate: The time and date the `Tweet` was read (in Epoch format) to be saved for this `Tweet`
+     - Parameter message: The message to be saved for this `Tweet`
+
+     */
     init (messageId: Int, createdTimeDate: Int, readTimeDate: Int, message: String) {
         self.messageId = messageId
         self.createdTimeDate = createdTimeDate
